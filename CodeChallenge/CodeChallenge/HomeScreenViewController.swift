3986dcd93da3fa832709ec4c888e7ca5f0c41eb4
Pravin Ghogare <pravin.ghogare@medtronic.com>
2020-06-27 17:04:49 +0530

    Add Initial Project and files

---
+++ CodeChallenge/CodeChallenge/HomeScreenViewController.swift
@@ -0,0 +1,47 @@
+//
+//  HomeScreenViewController.swift
+//  CodeChallenge
+//
+//  Created by Pravin Ghogare on 27/06/20.
+//  Copyright © 2020 own. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+
+}
---
+++ CodeChallenge/CodeChallenge/Model/DataItem.swift
@@ -0,0 +1,29 @@
+//
+//  DataItem.swift
+//  CodeChallenge
+//
+//  Created by Pravin Ghogare on 27/06/20.
+//  Copyright © 2020 own. All rights reserved.
+//
+
+import Foundation
+
+struct Country: Decodable {
+    
+    let id: String?
+    let type: String?
+    let date: String?
+    let data: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, type, date, data
+    }
+   
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decodeIfPresent(String.self, forKey: .id)
+        type = try values.decodeIfPresent(String.self, forKey: .type)
+        date = try values.decodeIfPresent(String.self, forKey: .date)
+        data = try values.decodeIfPresent(String.self, forKey: .data)
+    }
+}
